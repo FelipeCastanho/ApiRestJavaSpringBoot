03ddce014eec29b594a6e8dd91f79e97fbf2e68a
Felipe <castano.felipe.95@gmail.com>
2019-09-15 01:23:02 -0500

    First commit

---
+++ src/main/java/com/felipe/ApiRestJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.felipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestJavaSpringBootApplication.class, args);
+	}
+
+}
